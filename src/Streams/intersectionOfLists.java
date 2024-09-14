eee7c1d5b422fd8ed6b01e33365042429dbc2248
ACER <sumit.jaiswal8888@gmail.com>
2024-09-14 17:24:36 +0530

    second commit

---
+++ src/Streams/intersectionOfLists.java
@@ -0,0 +1,15 @@
+package Streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class intersectionOfLists {
+    public static void main(String[] args) {
+        List<Integer> list1 = Arrays.asList(1, 3, 5, 7, 9);
+        List<Integer> list2 = Arrays.asList(2, 4, 6, 8, 10, 5, 3, 1);
+        List<Integer> intersection=list1.stream().filter(list2::contains).collect(Collectors.toList());
+        System.out.println(intersection);
+    }
+
+}
---
+++ src/Streams/mergeTwoLists.java
@@ -0,0 +1,15 @@
+package Streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class mergeTwoLists {
+    public static void main(String[] args) {
+        List<Integer> list1= Arrays.asList(1, 3, 5, 7, 9);
+        List<Integer> list2=Arrays.asList(2, 4, 6, 8, 9,6, 10);
+        List<Integer> mergedlists= Stream.concat(list1.stream(),list2.stream()).sorted().collect(Collectors.toList());
+        System.out.println(mergedlists);
+    }
+}
